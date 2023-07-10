abf9ab0765969d8b50b97802fca5f404bc86041a
MatheusLucca <mat.lualves@gmail.com>
2023-07-10 19:54:45 -0300

    Payment one-to-one association

--- src/main/java/com/matlucca/webservice/config/TestConfig.java
+++ src/main/java/com/matlucca/webservice/config/TestConfig.java
@@ -69,6 +69,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3,p5,2,p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null,Instant.parse("2021-10-02T21:53:07Z"),o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 
 }
