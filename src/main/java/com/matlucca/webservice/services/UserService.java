e33edcfc96dacab0eef2e0ede0bcb4d9fc215e38
MatheusLucca <mat.lualves@gmail.com>
2023-07-11 18:55:50 -0300

    Exception Handler- FindById

--- src/main/java/com/matlucca/webservice/services/UserService.java
+++ src/main/java/com/matlucca/webservice/services/UserService.java
@@ -2,6 +2,7 @@ package com.matlucca.webservice.services;
 
 import com.matlucca.webservice.entities.User;
 import com.matlucca.webservice.repositories.UserRepository;
+import com.matlucca.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +21,8 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
-
     public User insert(User obj) {
         return repository.save(obj);
     }
