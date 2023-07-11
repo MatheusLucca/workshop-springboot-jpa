77da0b92cb12349964c1765c0519b561bcd8933e
MatheusLucca <mat.lualves@gmail.com>
2023-07-11 19:02:44 -0300

    Exception Handler - Delete

---
+++ src/main/java/com/matlucca/webservice/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.matlucca.webservice.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
